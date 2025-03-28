45b72630ac5295ede76cb02f52ac97311809f0e3
zinh8803 <ngoquocvinh2003@gmail.com>
2025-03-28 14:40:13 +0700

    add refesh token

--- backend/src/routes/UsersRoute.js
+++ backend/src/routes/UsersRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { createUser, getUser, updateUser, login, logout } = require('../controller/UsersController');
+const { createUser, getUser, updateUser, login, logout,refreshToken } = require('../controller/UsersController');
 const { authenticateToken } = require('../Middleware/Authencation');
 const router = express.Router();
 
 router.get('/', authenticateToken, getUser);
-router.post('/resgister', createUser);
+router.post('/register', createUser);
 router.put('/updateUser/', authenticateToken, updateUser);
-
+router.post('/refresh-token', refreshToken);
 router.post('/login', login)
 router.post('/logout', logout)
 module.exports = router;
