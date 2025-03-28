309565c3429e7353c445a3f4fa36c3547d28cff9
zinh8803 <ngoquocvinh2003@gmail.com>
2025-03-28 13:52:24 +0700

    jwt user

--- backend/src/routes/UsersRoute.js
+++ backend/src/routes/UsersRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createUser, getUser, updateUser } = require('../controller/UsersController');
+const { createUser, getUser, updateUser, login, logout } = require('../controller/UsersController');
+const { authenticateToken } = require('../Middleware/Authencation');
 const router = express.Router();
 
-router.get('/:id', getUser);
+router.get('/', authenticateToken, getUser);
 router.post('/resgister', createUser);
-router.put('/updateUser/:id', updateUser);
+router.put('/updateUser/', authenticateToken, updateUser);
 
+router.post('/login', login)
+router.post('/logout', logout)
 module.exports = router;
