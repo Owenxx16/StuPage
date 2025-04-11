607a6e0490d9ca50fcb070cc1f4ae3b53556e5b2
zinh8803 <ngoquocvinh2003@gmail.com>
2025-04-11 20:32:46 +0700

    add getall user

--- backend/src/routes/UsersRoute.js
+++ backend/src/routes/UsersRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createUser, getUser, updateUser, login, logout, refreshToken } = require('../controller/UsersController');
+const { createUser, getUser, updateUser, login, logout, refreshToken, getallusers } = require('../controller/UsersController');
 const { authenticateToken } = require('../Middleware/Authencation');
 const router = express.Router();
 
@@ -9,4 +9,5 @@ router.put('/updateUser/', authenticateToken, updateUser);
 router.post('/refresh-token', refreshToken);
 router.post('/login', login)
 router.post('/logout', logout)
+router.get('/getall', authenticateToken, getallusers)
 module.exports = router;
