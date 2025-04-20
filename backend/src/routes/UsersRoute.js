f5d3cd11c629009b639a6b1334e00d9673c7f62d
zinh8803 <ngoquocvinh2003@gmail.com>
2025-04-20 13:57:44 +0700

    get all user

--- backend/src/routes/UsersRoute.js
+++ backend/src/routes/UsersRoute.js
@@ -3,7 +3,7 @@ const { createUser, getUser, updateUser, login, logout, refreshToken, getalluser
 const { authenticateToken } = require('../Middleware/Authencation');
 const router = express.Router();
 
-// router.get('/', authenticateToken, getUser);
+router.get('/', authenticateToken, getUser);
 // router.post('/register', createUser);
 // router.put('/updateUser/', authenticateToken, updateUser);
 router.post('/refresh-token', refreshToken);
